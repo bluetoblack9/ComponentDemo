00029a8b4aff44cbf1f566860916c8d2164550c2
bluetoblack9 <760898237@qq.com>
2021-03-31 00:07:59 +0800

    组件库单工程调试配置demo

---
+++ news/src/main/java/com/blue/news/NewsActivity.java
@@ -0,0 +1,22 @@
+package com.blue.news;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * Created by blue on 2021/03/27.
+ * (*^▽^*)
+ * 描述:
+ */
+
+public class NewsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.news_activity);
+    }
+}
